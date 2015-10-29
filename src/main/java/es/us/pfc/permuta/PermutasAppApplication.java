fcb568a3e33c4c84e7c3229e60f6cc5bf814c4df
Pilar Domínguez <pilardv@gmail.com>
2015-10-30 00:56:27 +0100

    PERMUTAS-0000 Primera subida al repositorio

---
+++ src/main/java/es/us/pfc/permuta/PermutasAppApplication.java
@@ -0,0 +1,12 @@
+package es.us.pfc.permuta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermutasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermutasAppApplication.class, args);
+    }
+}
