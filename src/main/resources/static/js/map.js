2b380bd46043d6e83f7a9605a6a3b69f976f809d
Pilar Domínguez <pilardv@gmail.com>
2016-04-18 03:01:35 +0200

    Refactorización del código de home.html

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,19 +1,4 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 3,
-    styles: [{
-      featureType: 'poi',
-      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
-    }, {
-      featureType: 'transit.station',
-      stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
-    }],
-    disableDoubleClickZoom: true
-  });
-}  
- 
-  function initMapWithMarker() {
     var geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(37.3580015, -5.9873419);
     var mapOptions = {
@@ -23,7 +8,7 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
     
     var direccionPlazaActual = document.getElementById("direccionPlazaActual").value;
-    geocoder.geocode( { 'direccionPlazaActual': direccionPlazaActual}, function(results, status) {
+    geocoder.geocode( { 'address': direccionPlazaActual}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
@@ -33,5 +18,6 @@ function initMap() {
       } else {
         alert("Geocode was not successful for the following reason: " + status);
 	      }
-	    });
-  }
+	  });
+}
+google.maps.event.addDomListener(window, 'load', initMap);
