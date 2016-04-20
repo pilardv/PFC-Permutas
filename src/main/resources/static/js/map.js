76249e6847185953f6a22e1e3204f7a1b9d93769
Pilar Domínguez <pilardv@gmail.com>
2016-04-21 00:10:56 +0200

    Se añade en el mapa del usuario, página home, los marcadores de sus plazas deseadas.

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,23 +1,67 @@
 function initMap() {
     var geocoder = new google.maps.Geocoder();
+    
+    // Posicion de Sevilla
     var latlng = new google.maps.LatLng(37.3580015, -5.9873419);
+    
     var mapOptions = {
       zoom: 8,
       center: latlng
     }
+    
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
     
     var direccionPlazaActual = document.getElementById("direccionPlazaActual").value;
-    geocoder.geocode( { 'address': direccionPlazaActual}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-	      }
-	  });
+    var numPlazasDeseadas = document.getElementById("segmentoSel").childElementCount;
+    
+    if(null != direccionPlazaActual){
+    	geocoder.geocode( { 'address': direccionPlazaActual}, function(results, status) {
+    	      if (status == google.maps.GeocoderStatus.OK) {
+    	        map.setCenter(results[0].geometry.location);
+    	        
+    	        var miPlaza = new google.maps.Marker({
+    	            map: map,
+    	            position: results[0].geometry.location,
+    	            title: 'Mi plaza',
+    	            animation: google.maps.Animation.DROP
+    	        });
+    	        
+    	        miPlaza.setMap(map);
+    	        
+    	      } else {
+    	        alert("Geocode was not successful for the following reason: " + status);
+    		      }
+    		  });
+    }
+    
+    if(numPlazasDeseadas > 1){    	
+    	
+    	for(i=1; i < document.getElementById("segmentoSel").childElementCount ; i++){
+    		
+    		var direccionPlazaDeseada =  document.getElementById("segmentoSel")[i].innerText;
+    		
+    		geocoder.geocode( { 'address': direccionPlazaDeseada}, function(results, status) {
+      	      if (status == google.maps.GeocoderStatus.OK) {
+      	        map.setCenter(results[0].geometry.location);
+      	        
+      	        var plazaDeseada = new google.maps.Marker({
+//      	        	icon: {
+//      	        	  fillColor: '#00f'
+//      	        	},
+      	            position: results[0].geometry.location,
+      	            title: 'Plaza deseada',
+      	            map: map
+      	        });
+      	        
+      	        plazaDeseada.setMap(map);
+      	        
+      	      } else {
+      	        alert("Geocode was not successful for the following reason: " + status);
+      		      }
+      		  });
+    	}
+    	
+    }
+    
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+//google.maps.event.addDomListener(window, 'load', initMap);
